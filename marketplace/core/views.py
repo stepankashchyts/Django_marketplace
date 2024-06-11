49f2f926e3680be2f8c1396d0ebfbe043308526d
Hush <hushifierx@gmail.com>
2024-06-11 20:17:16 +0200

    initial commit

---
+++ marketplace/core/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+from item.models import Item, Category
+from .forms import SignUpForm
+
+# Create your views here.
+def index(request):
+    items = Item.objects.all()
+    categories=Category.objects.all()
+    return render(request, 'core/index.html', {'items':items, 'categories':categories})
+
+def contact(request):
+    return render(request, 'core/contact.html')
+
+def signup(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/login/')    
+    else: 
+        form = SignUpForm()
+    return render(request, 'core/signup.html', {'form':form})
---
+++ marketplace/marketplace/urls.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('', include('core.urls')),
+    path('admin/', admin.site.urls),
+    path('items/', include('item.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
